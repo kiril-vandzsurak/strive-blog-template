99782bfc797b0c032e6e4c2efeaea2b9429d9c34
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-12-21 16:27:54 +0100

    Concatinating API with start page

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,11 +2,32 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
+import { useState, useEffect } from "react";
 
 const BlogList = (props) => {
+  const [info, setInfo] = useState([]);
+
+  const fetchBlogs = async () => {
+    try {
+      let response = await fetch(`http://localhost:3001/authors`);
+      if (response.ok) {
+        let data = await response.json();
+        setInfo(data);
+      } else {
+        console.log("error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
   return (
     <Row>
-      {posts.map((post) => (
+      {info.map((post) => (
         <Col
           md={4}
           style={{
